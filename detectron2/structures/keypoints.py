4fb6211468faaf472eea690491574089ab91e7c6
edanpa-bw <edan.pa@bluewhite.ai>
2025-02-03 10:07:02 +0200

    Added and tested MLflow hook that seems to work well.
    Need to modify our mlflow wrapper that will work well for all of our training projects

--- detectron2/structures/keypoints.py
+++ detectron2/structures/keypoints.py
@@ -26,6 +26,8 @@ class Keypoints:
                 instances, and K is the number of keypoints per instance.
         """
         device = keypoints.device if isinstance(keypoints, torch.Tensor) else torch.device("cpu")
+        if isinstance(keypoints, List):
+            keypoints = np.array(keypoints)
         keypoints = torch.as_tensor(keypoints, dtype=torch.float32, device=device)
         assert keypoints.dim() == 3 and keypoints.shape[2] == 3, keypoints.shape
         self.tensor = keypoints
